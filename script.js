13d320391afff0fbdcb1be9729c4864b210cd437
unknown <slashley127@gmail.com>
2022-07-02 10:28:36 -0500

    first edits

--- script.js
+++ script.js
@@ -2,6 +2,19 @@
 
 window.addEventListener("load", function() {
 
+   let form = document.getElementById("launchForm");
+   let pilotName = document.getElementById("pilotName");
+   let copilotName = document.getElementsByName("copilotName");
+   let 
+
+
+   form.addEventListener("submit", (event) => {
+    console.log("test?");
+    event.preventDefault();
+   });
+
+
+
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse;
@@ -12,5 +25,7 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
-   
+
+
+
 });
